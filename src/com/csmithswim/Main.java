8626cdc70296a0748e5b87af2ce5475196902a05
Cullen S <cullensmi@gmail.com>
2020-10-15 11:17:43 -0400

    Reverse a string first commit

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -7,14 +7,28 @@ it the Fahrenheit temperature equivalent to the given Celsius temperature.
 Use the algorithm mentioned above to help convert the Celsius temperature to Fahrenheit.*/
 
 public class Main {
+/* Reverse the provided string. You may need to turn the string into an array before you can reverse it.
+Your result must be a string.
+ReverseString("hello") should return a string.
+reverseString("hello") should become "olleh".
+reverseString("Howdy") should become "ydwoH".
+reverseString("Greetings from Earth") should return "htraE morf sgniteerG".
+ */
+    //Method to reverse string
+    public static String reverseAString(String word){
+
+        return word;
+
+    }
 
        public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("This program converts Celsius to Fahrenheit. Please enter Celsius for conversion.\nCelsius: ");
+        System.out.print("This program reverses a word, please enter word to be reversed.\nWord: ");
 
-        float input = scanner.nextFloat();
+        String input = scanner.next();
         System.out.println(input);
-
     }
+
+
 }
 
