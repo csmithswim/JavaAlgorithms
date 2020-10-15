a857f421a6ec1bbaa212ec472d242bcf5c194fdf
Cullen S <cullensmi@gmail.com>
2020-10-15 19:09:23 -0400

    Solved Reverse A String

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -1,10 +1,5 @@
 package com.csmithswim;
 import java.util.Scanner;
-/*
-The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
-You are given a variable celsius representing a temperature in Celsius. Use the variable fahrenheit already defined and assign
-it the Fahrenheit temperature equivalent to the given Celsius temperature.
-Use the algorithm mentioned above to help convert the Celsius temperature to Fahrenheit.*/
 
 public class Main {
 /* Reverse the provided string. You may need to turn the string into an array before you can reverse it.
@@ -14,21 +9,25 @@ reverseString("hello") should become "olleh".
 reverseString("Howdy") should become "ydwoH".
 reverseString("Greetings from Earth") should return "htraE morf sgniteerG".
  */
-    //Method to reverse string
-    public static String reverseAString(String word){
+//    Method to reverse string using a for loop and substring method
 
-        return word;
+    public static String reverseString(String word){
 
+        String reversedString = "";
+
+        for (int i = word.length()-1; i >= 0; i--){
+
+            reversedString += word.substring(i,i+1);
+        }
+        System.out.println(reversedString);
+        return reversedString;
     }
 
-       public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("This program reverses a word, please enter word to be reversed.\nWord: ");
-
-        String input = scanner.next();
-        System.out.println(input);
+        String input = scanner.nextLine();
+        reverseString(input);
     }
-
-
 }
 
