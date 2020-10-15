763d35950ec277963c14f4b02b5bce8cb5db2bd9
Cullen S <cullensmi@gmail.com>
2020-10-15 10:53:54 -0400

    Celsius to Fahreinheit first commit

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -1,18 +1,20 @@
 package com.csmithswim;
-
-//Create a method that takes a string and returns the word count. The string will be a sentence.
-//countWords("Just an example here move along") ➞ 6
-//countWords("This is a test") ➞ 4
-//countWords("What an easy task, right") ➞ 5
+import java.util.Scanner;
+/*
+The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
+You are given a variable celsius representing a temperature in Celsius. Use the variable fahrenheit already defined and assign
+it the Fahrenheit temperature equivalent to the given Celsius temperature.
+Use the algorithm mentioned above to help convert the Celsius temperature to Fahrenheit.*/
 
 public class Main {
 
-    public static String stringLength(String input){
-        System.out.println(input.length());
-        return input;
-    }
+       public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("This program converts Celsius to Fahrenheit. Please enter Celsius for conversion.\nCelsius: ");
 
-    public static void main(String[] args) {
-    stringLength("joe");
+        float input = scanner.nextFloat();
+        System.out.println(input);
+
     }
 }
+
