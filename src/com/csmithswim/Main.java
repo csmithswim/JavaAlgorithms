bfe875bd5bf79de7b8a1a63ed8e87991ca6811cb
Cullen S <cullensmi@gmail.com>
2020-10-16 12:45:20 -0400

    Solved factorialize

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -13,14 +13,26 @@ factorialize(20) should return 2432902008176640000.
 factorialize(0) should return 1.
 Only integers greater than or equal to zero will be supplied to the function.
  */
-    public static int factorialize(int number){
+    public static double factorialize(double number){
 
+        double count = 1;
+        double factorial = 1;
+
+        while (count < number+1){
+
+            factorial *= count;
+            count++;
+
+        }
+        System.out.println(factorial);
+        return factorial;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("");
-        String input = scanner.nextLine();
+        System.out.print("Input number for its factorial to be computed: ");
+        double input = scanner.nextDouble();
+        factorialize(input);
     }
 }
 
