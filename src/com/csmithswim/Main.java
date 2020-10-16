5c2728d52ec2bf7957a9a5cb7223cdcd52c91fdc
Cullen S <cullensmi@gmail.com>
2020-10-16 12:50:39 -0400

    Find longest word in sentence first commit

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -2,37 +2,19 @@ package com.csmithswim;
 import java.util.Scanner;
 
 public class Main {/*
-Return the factorial of the provided integer.If the integer is represented with the letter n,
-a factorial is the product of all positive integers less than or equal to n.
-Factorials are often represented with the shorthand notation n!
-For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
-factorialize(5) should return a number.
-factorialize(5) should return 120.
-factorialize(10) should return 3628800.
-factorialize(20) should return 2432902008176640000.
-factorialize(0) should return 1.
-Only integers greater than or equal to zero will be supplied to the function.
+Return the length of the longest word in the provided sentence.
+Your response should be a number.
+findLongestWordLength("The quick brown fox jumped over the lazy dog") should return a number.
+findLongestWordLength("The quick brown fox jumped over the lazy dog") should return 6.
+findLongestWordLength("May the force be with you") should return 5.
+findLongestWordLength("Google do a barrel roll") should return 6.
+findLongestWordLength("What is the average airspeed velocity of an unladen swallow") should return 8.
+findLongestWordLength("What if we try a super-long word such as otorhinolaryngology") should return 19.
  */
-    public static double factorialize(double number){
-
-        double count = 1;
-        double factorial = 1;
-
-        while (count < number+1){
-
-            factorial *= count;
-            count++;
-
-        }
-        System.out.println(factorial);
-        return factorial;
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Input number for its factorial to be computed: ");
-        double input = scanner.nextDouble();
-        factorialize(input);
+        System.out.print("This program will return the the length of the longest word in a given sentence.\nPlease input sentence to start:");
+
     }
 }
 
